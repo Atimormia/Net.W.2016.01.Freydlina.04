0917368f1afcc007c86b5cb7c8aca5743a7c91be
atimormia <atimormia@gmail.com>
2016-10-27 20:33:17 +0300

    Fixed Calculating. Added testing throws

---
+++ NewtonTests/NewtonTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Task1;
+
+namespace NewtonTests
+{
+    [TestFixture]
+    public class NewtonTests
+    {
+        #region Test method: public static double NewtonSqrt(double value)
+        public static IEnumerable<TestCaseData> TestCasesForNewtonNthSqrt
+        {
+            get
+            {
+                yield return new TestCaseData(4, 2).Returns(Math.Sqrt(4));
+                yield return new TestCaseData(3, 2).Returns(Math.Sqrt(3));
+                yield return new TestCaseData(1,1).Returns(1);
+                yield return new TestCaseData(int.MaxValue,2).Returns(Math.Sqrt(int.MaxValue));
+                yield return new TestCaseData(Math.Pow(1234567,4),4).Returns(1234567);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCasesForNewtonNthSqrt))]
+        public double TestNewtonSqrt(double value, int degree)
+        {
+            return Newton.NewtonNthSqrt(value,degree);
+
+        }
+
+        public static IEnumerable<TestCaseData> TestCasesForNewtonNthSqrtThrows
+        {
+            get
+            {
+                yield return new TestCaseData(-1, 2);
+                yield return new TestCaseData(0, 2);
+                yield return new TestCaseData(5, 0);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCasesForNewtonNthSqrtThrows))]
+        public void TestNewtonSqrtThrows(double value, int degree)
+        {
+            Assert.That(() => Newton.NewtonNthSqrt(value, degree), Throws.TypeOf<ArgumentException>());
+
+        }
+        #endregion
+
+    }
+}
---
+++ Task1/Newton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Newton
+    {
+        private const double e = 0.0000000000000001;
+
+        public static double NewtonNthSqrt(double value, int degree)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Value must be positive");
+            }
+            if (degree == 0)
+            {
+                throw new ArgumentException("Degree must not be 0");
+            }
+            int degreeAbs = Math.Abs(degree);
+            bool degreeIsNegative = degree != degreeAbs;
+            degree = degreeAbs;
+
+            double result = value;
+            double accuracy = 1;
+            while (accuracy >= e)
+            {
+                double currentValue = ((degree - 1) * result + value / Math.Pow(result, degree - 1)) / degree;
+                accuracy = Math.Abs(result - currentValue);
+                result = currentValue;
+            }
+
+            if (degreeIsNegative)
+            {
+                return Math.Round(1d/result, 10);
+            }
+            return result;
+        }
+    }
+}
